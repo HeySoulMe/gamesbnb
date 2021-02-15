3e71172f9c700b632ad9e3877c75ae5166855bc7
Edouard Belfond <edouard.belfond@grenoble-em.com>
2021-02-15 16:11:42 +0100

    added models/migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_15_141959) do
+ActiveRecord::Schema.define(version: 2021_02_15_150925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "boardgames", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "category"
+    t.integer "players"
+    t.float "price"
+    t.bigint "user_id", null: false
+    t.string "address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_boardgames_on_user_id"
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "start_at"
+    t.datetime "end_at"
+    t.bigint "user_id", null: false
+    t.bigint "boardgame_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["boardgame_id"], name: "index_bookings_on_boardgame_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +54,7 @@ ActiveRecord::Schema.define(version: 2021_02_15_141959) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "boardgames", "users"
+  add_foreign_key "bookings", "boardgames"
+  add_foreign_key "bookings", "users"
 end
