77e41f6ee5e228f5394eadad130c7b671707218f
Edouard Belfond <edouard.belfond@grenoble-em.com>
2021-02-16 16:08:26 +0100

    add_through_to_the_user_model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_one_attached :photo
   has_many :bookings
+  has_many :bookings_as_owner, through: :boardgames, source: :bookings
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
