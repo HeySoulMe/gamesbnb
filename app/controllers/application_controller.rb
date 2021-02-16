0c20a65260b7a7fa3754448fcfa84616644767e4
Alexandra Canuel <alexcanuel@gmail.com>
2021-02-16 10:50:36 +0100

    install pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,17 +15,18 @@ class ApplicationController < ActionController::Base
   #   redirect_to(root_path)
   # end
 
-  private
 
-  def skip_pundit?
-    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
-  end
-  
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username])
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
+
+  private
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 end
