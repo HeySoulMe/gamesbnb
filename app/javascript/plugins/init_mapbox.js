e57292134cbab2ceacad05220690215e64bcbee6
Javier Cuadrado <mantecajc@gmail.com>
2021-02-17 11:29:35 +0100

    Added pop up

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -16,13 +16,16 @@ const initMapbox = () => {
     });
     const markers = JSON.parse(mapElement.dataset.markers);
 	  markers.forEach((marker) => {
+	  	const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
 	    new mapboxgl.Marker()
 	      .setLngLat([ marker.lng, marker.lat ])
+	      .setPopup(popup) // add this
 	      .addTo(map);
 	  });
 	  fitMapToMarkers(map, markers);
   }
 };
+
 
 
 export { initMapbox };
