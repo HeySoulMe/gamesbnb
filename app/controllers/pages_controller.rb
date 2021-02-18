061c995b391cd1c20b053c76a74d36f2d2d76588
Edouard Belfond <edouard.belfond@grenoble-em.com>
2021-02-18 18:02:43 +0100

    bookings status update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,5 +6,7 @@ class PagesController < ApplicationController
 
   def dashboard
   	@bookings = current_user.bookings
+    @my_boardgames = current_user.boardgames
+    @bookings_as_owner = current_user.bookings_as_owner
   end
 end
